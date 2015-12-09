a62f6f778cb6ec0b69c5ede44df41af81372f27e
kg <kylgrvs@gmail.com>
2015-12-09 14:58:34 -0800

    completes step 8
    includes:
    - offloads work to v8 thread pool

--- myaddon/index.js
+++ myaddon/index.js
@@ -1,6 +1,12 @@
-var bindings = require('bindings');
-var addon = bindings('myaddon');
+var addon = require('bindings')('myaddon')
 
-addon.delay(process.argv[2], function() {
-  console.log('Done!');
-});
+var interval = setInterval(function () {
+  process.stdout.write('.')
+}, 50)
+
+addon.delay(process.argv[2], function () {
+  clearInterval(interval)
+  console.log('Done!')
+})
+
+process.stdout.write('Waiting')
--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -1,5 +1,4 @@
 #include <nan.h>
-#include <iostream>
 
 // check if on linux
 #ifndef _WIN32
@@ -8,18 +7,60 @@
 
 using namespace v8;
 
+// A worker class extending the NanAsyncWorker helper
+// class, a simple encapsulation of worker-thread
+// logic to make simple tasks easier
+
+class MyWorker : public Nan::AsyncWorker {
+ public:
+  // Constructor
+  MyWorker(Nan::Callback *callback, int delay)
+    : Nan::AsyncWorker(callback), delay(delay) {}
+  // Destructor
+  ~MyWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    // Asynchronous, non-V8 work goes here
+
+    // windows vs POSIX-compliant check
+    #ifdef _WIN32
+      Sleep(delay);
+    #else
+      usleep(delay * 1000);
+    #endif
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    Nan::HandleScope scope;
+
+    // Nan::Callback#Call() does a Nan::MakeCallback() for us
+    callback->Call(0, NULL);
+  }
+
+ private:
+  int delay;
+};
+
 NAN_METHOD(Delay) {
-  int num = info[0]->IntegerValue();
-
-  // windows vs POSIX-compliant check
-  #ifdef _WIN32
-  Sleep(num);
-  #else
-  usleep(num * 1000);
-  #endif
-
+  // get delay and callback
+  int delay = info[0]->IntegerValue();
   v8::Local<Function> callback = info[1].As<Function>();
-  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), callback, 0, NULL);
+
+  // create NanCallback instance wrapping the callback
+  Nan::Callback* nanCallback = new Nan::Callback(callback);
+
+  // create a MyWorker instance, passing the callback and delay
+  MyWorker* worker = new MyWorker(nanCallback, delay);
+
+  // queue the worker instance onto the thread-pool
+  Nan::AsyncQueueWorker(worker);
 }
 
 NAN_MODULE_INIT(Init) {
