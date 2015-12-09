616254df8aae8b859d02936df5908e3cac10bb1a
kg <kylgrvs@gmail.com>
2015-12-09 12:20:38 -0800

    completes step 6
    includes:
    - adding a length function in native addon

--- myaddon/myaddon.cc
+++ myaddon/myaddon.cc
@@ -2,19 +2,22 @@
 
 using namespace v8;
 
-NAN_METHOD(Print) {
+NAN_METHOD(Length) {
   Nan::MaybeLocal<String> maybeStr = Nan::To<String>(info[0]);
   v8::Local<String> str;
+
   if(maybeStr.ToLocal(&str) == false) {
     Nan::ThrowError("Error converting first argument to string");
   }
 
-  printf("%s\n", *String::Utf8Value(str));
+  int len = strlen(*String::Utf8Value(str));
+
+  info.GetReturnValue().Set(len);
 }
 
 NAN_MODULE_INIT(Init) {
-  Nan::Set(target, Nan::New("print").ToLocalChecked(),
-      Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+  Nan::Set(target, Nan::New("length").ToLocalChecked(),
+      Nan::GetFunction(Nan::New<FunctionTemplate>(Length)).ToLocalChecked());
 }
 
-NODE_MODULE(myaddon, Init);
+NODE_MODULE(myaddon, Init)
